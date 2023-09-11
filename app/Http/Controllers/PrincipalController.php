79ee54cf91a8a321758f38e2d241231895fd8fde
Juubaju <juszpr@gmail.com>
2023-09-11 08:55:16 -0300

    first commit

---
+++ app/Http/Controllers/PrincipalController.php
@@ -0,0 +1,27 @@
+<?php
+
+ 
+
+namespace App\Http\Controllers;
+
+ 
+
+use Illuminate\Http\Request;
+
+ 
+
+class PrincipalController extends Controller
+
+{
+
+    public function principal()
+
+    {
+
+        $mensagem = "Esta é uma mensagem para a página principal.";
+
+        return view('principal', compact('mensagem'));
+
+    }
+
+}
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,18 @@
+<h3>PRINCIPAL (view) </h3>
+
+<ul>
+    <li>
+        <a href="/"> PRINCIPAL </a>
+        </li>
+
+        <ul>
+    <li>
+        <a href="/sobrenos"> SOBRE NÓS </a>
+        </li>
+
+        <ul>
+    <li>
+        <a href="/contato"> CONTATO </a>
+        </li>
+
+</ul>
